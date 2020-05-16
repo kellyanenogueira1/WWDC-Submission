314f86d700fd3a8b2c57e6b91a63b81045dd44cb
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-16 12:22:00 -0300

    Animação de imagens

--- Wwdc.playgroundbook/Edits/UserEdits.diffpack/UserModules/AccessibiltyViewController.playgroundmodule/Sources/AccessibilityViewController.swift
+++ Wwdc.playgroundbook/Edits/UserEdits.diffpack/UserModules/AccessibiltyViewController.playgroundmodule/Sources/AccessibilityViewController.swift
@@ -1,13 +1,44 @@
 // Code inside modules can be shared between pages and other source files.
 import UIKit
 
+var imageAnimation = UIImageView()
+var images: [UIImage] = []
+
 public class AccessibilityViewController : UIViewController {
     override public func loadView() {
         let view = UIView()
         self.view = view
-        
         view.backgroundColor = .white
-        
+        addImageInArray()
+        animation()
+    }
+    
+    public func imageConstraints(_ img: UIImage){
+        view.addSubview(imageAnimation)
+        imageAnimation.image = img
+        imageAnimation.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageAnimation.widthAnchor.constraint(equalToConstant: 400),
+            imageAnimation.heightAnchor.constraint(equalToConstant: 300),
+            imageAnimation.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100),
+            imageAnimation.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
+    }
+    
+    public func addImageInArray(){
+        for count in 0...10{
+            let strImageName = "img\(count).png"
+            let img = UIImage(named: strImageName)
+            images.append(img!)
+            imageConstraints(img!)
+        }
+    }
+    
+    public func animation(){
+        imageAnimation.animationImages = images
+        imageAnimation.animationDuration = 2.0
+        imageAnimation.animationRepeatCount = 1
+        imageAnimation.startAnimating()
     }
 }
 
--- Wwdc.playgroundbook/Edits/UserEdits.diffpack/UserModules/Conclusion.playgroundmodule/Sources/Conclusion.swift
+++ Wwdc.playgroundbook/Edits/UserEdits.diffpack/UserModules/Conclusion.playgroundmodule/Sources/Conclusion.swift
@@ -2,15 +2,23 @@
 import UIKit
 
 public class Conclusion : UIViewController {
-    var image = UIImageView
+    var imageView = UIImageView()
     
     override public func loadView() {
         let view = UIView()
         self.view = view
         
-        
-        
-            //view.
-        //view.backgroundColor = .white
+        imgConstraints()
+    }
+    func imgConstraints(){
+        view.addSubview(imageView)
+        imageView.image = #imageLiteral(resourceName: "backgroundPng.png")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0),
+            imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
+            imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0)
+        ])
     }
 }
