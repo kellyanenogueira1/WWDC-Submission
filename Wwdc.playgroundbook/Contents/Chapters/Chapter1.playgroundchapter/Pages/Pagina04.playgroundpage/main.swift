385a2633b8f9862c5c296a442f9cd529c7a8a455
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-17 22:05:24 -0300

    Página conclusion

--- Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina04.playgroundpage/main.swift
+++ Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina04.playgroundpage/main.swift
@@ -2,4 +2,9 @@
 import PlaygroundSupport
 import BookCore
 PlaygroundPage.current.liveView = Conclusion()
+PlaygroundPage.current.assessmentStatus = .pass(message: "It was great to spend time with you. Success!")
 //#-end-hidden-code
+/*:
+ #Sobre mim
+ 
+*/
--- Wwdc.playgroundbook/Contents/Modules/BookCore.playgroundmodule/Sources/ConclusionPage.swift
+++ Wwdc.playgroundbook/Contents/Modules/BookCore.playgroundmodule/Sources/ConclusionPage.swift
@@ -4,12 +4,14 @@ import UIKit
 
 public class Conclusion : UIViewController {
     var imageView = UIImageView()
+    var textConclusion = UITextView()
     
     override public func loadView() {
         let view = UIView()
         self.view = view
         
         imgConstraints()
+        formattingText()
     }
     func imgConstraints(){
         view.addSubview(imageView)
@@ -21,5 +23,17 @@ public class Conclusion : UIViewController {
             imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0),
             imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0)
         ])
+    }
+    public func formattingText(){
+        view.addSubview(textConclusion)
+        textConclusion.frame = CGRect(x: 80, y: 150, width: 430, height: 400)
+        textConclusion.isEditable = false
+        textConclusion.isSelectable = false
+        textConclusion.textAlignment = NSTextAlignment.center
+        textConclusion.font =  UIFont(name: "SF Compact Rounded", size: 22)
+        textConclusion.text = "Congratulations! I am happy that we have learned so much together. If you liked and want to know more about the topic, you can learn about the four categories of approach covered by the Human Interface Guidelines, which are: Vision, Hearing, Physics and Motor, Literacy and Learning. This was one of my references to carry out this project. I used some Shape.so icons that made the interface more beautiful."
+        textConclusion.backgroundColor = #colorLiteral(red: 0.9764705882352941, green: 0.8509803921568627, blue: 0.5490196078431373, alpha: 1.0)
+        textConclusion.layer.cornerRadius = 10
+        textConclusion.textColor = .black
     }
 }
