92dd334965a430156084c55479b9750df6ddc01a
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-17 23:14:17 -0300

    Página accessibility

--- Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina01.playgroundpage/main.swift
+++ Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina01.playgroundpage/main.swift
@@ -4,5 +4,16 @@ import BookCore
 PlaygroundPage.current.liveView = AccessibilityViewController()
 PlaygroundPage.current.assessmentStatus = .pass(message: "Go ahead to learn a little more! [Enlarge Text](@next)")
 //#-end-hidden-code
-
-
+/*:
+# Accessibility: we are equal because we are different
+Hello, I am Kellyane Nogueira, a Computer Science student and I would like to tell you a little about what I have learned in the last few days. Have you ever stopped to think about how different we are from each other? We have preferences, desires, dreams, principles, favorite colors, favorite movies... We are tall, short, curly, straight, blond, red, dark... and we still have things more specific to our individuality and it also affects the way we we interact with apps and devices.
+ 
+So making your app suitable for as many people as possible can be a difficult task, especially when you understand that for some people, finding simple, clear content can be even more difficult than for others. Thinking about it, I started to study more about accessibility. But what would that be?
+ 
+- Note
+ In a brief survey, the dictionary gives us the definition of accessibility as "character of what is accessible", "ease of approaching or acquiring something".
+ 
+ When it comes to an application, we can understand accessibility as the ease with which someone can understand our content and / or the message we want to convey.
+ Different people face different levels of difficulty when using devices and apps and we have the power to help them by adding some simple features to our projects.
+ 
+*/
--- Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina02.playgroundpage/main.swift
+++ Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina02.playgroundpage/main.swift
@@ -3,3 +3,9 @@ import PlaygroundSupport
 import BookCore
 PlaygroundPage.current.liveView = EnlargeTextViewController()
 //#-end-hidden-code
+/*:
+- Important:
+    "The beginning is the most important part of the work."
+\
+– Plato
+*/
