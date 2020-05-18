6d399a7588f15c98bc87eb57421f0257d754b275
kellyanenogueira1 <kellyanenogueira1@hotmail.com>
2020-05-17 23:46:37 -0300

    Página enlarge text

--- Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina02.playgroundpage/main.swift
+++ Wwdc.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Pagina02.playgroundpage/main.swift
@@ -4,8 +4,22 @@ import BookCore
 PlaygroundPage.current.liveView = EnlargeTextViewController()
 //#-end-hidden-code
 /*:
+ # Enlarging Text
+ 
+Vision is a very important sense to pay attention to when thinking about an accessible app. One of the tips I want to comment on is the increase in letters and characters.
+
 - Important:
-    "The beginning is the most important part of the work."
+    Vision problems can be diverse and, according to an IBGE survey, about 19% of the world population has some type of visual impairment, which impairs many of their daily tasks.
 \
-– Plato
+\
+– Folha de S. Paulo, 2019
+
+People with low vision need optical help, such as glasses, simple magnifying glasses, among other things, and also non-optical help, such as enlarged texts. That's right, a simple feature can make a lot of difference, but for you to understand better, how about experimenting?
+ 
+ * Experiment:
+    1. Identify the magnifying glass on the opposite page
+    2. Click on the magnifying glass
+    3. Repeat step "2" three more times to see the different sizes
+
+For the different sizes of this text, I used part of the accessibility category used by Apple on its devices.
 */
